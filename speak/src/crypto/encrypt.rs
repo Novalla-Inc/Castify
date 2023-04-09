85c9a90ec402f604bf70e4a6c7b648cd256dbe21
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-09 16:04:46 -0500

    added the ability to return project data

--- speak/src/crypto/encrypt.rs
+++ speak/src/crypto/encrypt.rs
@@ -2,7 +2,7 @@ use crypto_hash::{hex_digest, Algorithm};
 
 use serde_json;
 
-use crate::data::data::SaveData;
+use crate::data::data::{ProjectData, SaveData};
 
 /// Convert the value into a hash value -> sha256
 pub fn create_hash_value(value: &str) -> String {
@@ -20,7 +20,6 @@ pub fn remove_hash_value(value: String) -> String {
 
 /// Return the config data in a json format.
 pub fn return_config_data(config_name: String, project_name: String) -> String {
-	// TODO: implement this
 	let _cwd = std::env::current_dir();
 	let _cwd1 = std::env::current_dir();
 	let _config_path = format!(
@@ -37,6 +36,31 @@ pub fn return_config_data(config_name: String, project_name: String) -> String {
 	let _return_data = serde_json::to_string_pretty(&_data).unwrap();
 
 	return _return_data;
+}
+
+/// Return porject data as json
+pub fn return_project_data(project_name: String) -> String {
+	let _cwd = std::env::current_dir();
+	// project .yml file
+	let _project_path = format!(
+		"{}/projects/{}/{}.yml",
+		_cwd.unwrap().to_string_lossy(),
+		project_name,
+		project_name,
+	);
+
+	let _config_file = std::fs::File::open(_project_path).unwrap();
+
+	let _data: ProjectData = serde_yaml::from_reader(_config_file).unwrap();
+
+	let _return_data = serde_json::to_string_pretty(&_data).unwrap();
+
+	return _return_data;
+}
+
+#[test]
+fn test_return_project_data() {
+	assert!(return_project_data("test").len() != 0);
 }
 
 #[test]
