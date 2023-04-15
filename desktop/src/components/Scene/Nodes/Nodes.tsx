5b1745fdb6574f3745791e333676cf0c1cfc3dd1
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-15 17:36:11 -0500

    added different Node Type w/ conditional rendering; soon to add actually correct rendering

--- desktop/src/components/Scene/Nodes/Nodes.tsx
+++ desktop/src/components/Scene/Nodes/Nodes.tsx
@@ -1,22 +1,58 @@
 import styled from 'styled-components';
 
+type NodeProps = {
+    name: string;
+    color?: string;
+};
+
 const Node = styled.div`
     width: 10rem;
     height: 10rem;
+    padding: .25rem;
     background-color: white;
     color: white;
     border: 1px solid white;
     border-radius: .25rem; 
 `;
 
-const ChatNode = styled(Node)<{ name: string }>`
-    background-color: blue;
-    color: ${props => props.name || 'black'}
+const NodeTitle = styled.span`
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: black;
 `;
 
-const CameraNode = styled(Node)<{ name: string }>`
-    background-color: red;
-    color: ${props => props.name || 'black'}
+const NodeBody = styled.div`
+    font-size: 1rem;
+    font-weight: 400;
+    color: black;
 `;
+
+const ChatNodeTemplate = styled(Node)<{ color: string }>`
+    background-color: blue;
+    color: ${props => props.color || 'black'}
+`;
+
+const CameraNodeTemplate = styled(Node)<{ color: string }>`
+    background-color: red;
+    color: ${props => props.color || 'black'}
+`;
+
+function CameraNode ({ ...props }: NodeProps) {
+    return (
+        <CameraNodeTemplate color={''}>
+            <NodeTitle>{props.name}</NodeTitle>
+            <NodeBody>Camera Node</NodeBody>
+        </CameraNodeTemplate>
+    )
+} 
+
+function ChatNode ({ ...props }: NodeProps) {
+    return (
+        <ChatNodeTemplate color={''}>
+            <NodeTitle>{props.name}</NodeTitle>
+            <NodeBody>Chat Node</NodeBody>
+        </ChatNodeTemplate>
+    )
+}
 
 export { ChatNode, CameraNode };
