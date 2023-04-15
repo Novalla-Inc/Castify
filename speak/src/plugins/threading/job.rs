8598ad51e120668eb657b0e6af6fcaa904706af0
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-14 22:56:15 -0500

    removed test in threading crate; added env_logger; added logger crate to use a the debug logger;

--- speak/src/plugins/threading/job.rs
+++ speak/src/plugins/threading/job.rs
@@ -1,13 +1,13 @@
-use uuid::Uuid;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::thread;
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum JobType {
-    BACKGROUND,
-    FRONT,
-    RUNNING,
-    DEFAULT,
+	BACKGROUND,
+	FRONT,
+	RUNNING,
+	DEFAULT,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -26,43 +26,34 @@ pub struct Job {
 }
 
 pub struct JobHandler {
-    id: String,
-		job: Job,
-		job_method: fn (),
+	id: String,
+	job: Job,
+	job_method: fn(),
 }
 
 impl JobHandler {
-
-	 fn start_job(job_todo: fn()) {
-			// if the job has no data dependencies.
-			thread::spawn(move || {
-				job_todo();
-			});
-    }
-
+	fn start_job(job_todo: fn()) {
+		// if the job has no data dependencies.
+		thread::spawn(move || {
+			job_todo();
+		});
+	}
 }
 
+pub fn define_job_system(j_type: JobType, job_todo: fn()) -> JobHandler {
+	let _new_handler: JobHandler = JobHandler {
+		id: Uuid::new_v4().to_string(),
+		job: Job {
+			job_name: "test".to_string(),
+			job_status: JobStatus::STARTING,
+			job_type: j_type,
+		},
+		job_method: job_todo,
+	};
 
-
-pub fn define_job_system(j_type: JobType, job_todo: fn ()) -> JobHandler {
-    let _new_handler: JobHandler = JobHandler {
-			id: Uuid::new_v4().to_string(),
-			job: Job {
-				job_name: "test".to_string(),
-				job_status: JobStatus::STARTING,
-				job_type: j_type,
-			},
-			job_method: job_todo,
-		};
-
-		return _new_handler;
+	return _new_handler;
 }
 
 fn print_test() {
 	println!("Test");
-}
-
-#[test]
-fn test_define_job_system () {
-	println!("{:?}", define_job_system(JobType::DEFAULT, print_test()));
 }
