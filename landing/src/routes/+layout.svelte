ed927e45c1f6c9039c9c5a43369da7f3b6144382
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-21 14:21:33 -0500

    added landing scafold

---
+++ landing/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import '../css/core.css';
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/docs">Docs</a>
+</nav>
+
+<slot></slot>
---
+++ landing/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Home Page</h1>
+
+<style lang="postcss">
+    /* :global(html) {
+        background-color: theme(colors.green.200);
+    } */
+</style>
