e56c349fa7114e8a8723031bd867436c6fbd197a
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-21 19:54:08 -0500

    started work on the landing home page

--- landing/src/routes/+page.svelte
+++ landing/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="text-6xl">Home Page</h1>
 
+<div class="p-2">
+    <h1 class="heading">A Streaming Tool for Creators.</h1>
+    <p class="paragraph">Effortlessly broadcast your message with Castify.</p>
+</div>
