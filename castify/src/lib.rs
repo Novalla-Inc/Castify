f3b276a1f2db95e3a9fbe1620b1354818eaa90ea
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-21 20:59:04 -0500

    format

--- castify/src/lib.rs
+++ castify/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod crypto;
 pub mod data;
+pub mod plugins;
 pub mod router;
 pub mod util;
-pub mod plugins;
 // TODO: Fix router.
 // pub mod core_logger;
--- castify/src/plugins/threading/file.rs
+++ castify/src/plugins/threading/file.rs
@@ -4,12 +4,12 @@ type Task = fn();
 
 /// Create a new thread to create a new file then close the thread after file creation.
 pub fn create_file(task: Task) -> String {
-    let new_thread = thread::spawn(move || {
-        // Task is a function pointer.
-        task();
-    });
+	let new_thread = thread::spawn(move || {
+		// Task is a function pointer.
+		task();
+	});
 
-    new_thread.join().unwrap();
+	new_thread.join().unwrap();
 
-    return "Thread Finished.".to_string();
+	return "Thread Finished.".to_string();
 }
