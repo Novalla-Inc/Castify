d4fc11ccf92be47a2b0081d03802a631a38bc26e
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-23 14:14:52 -0500

    added context menu

---
+++ desktop/src/components/General/ContextMenu.tsx
@@ -0,0 +1,9 @@
+/**
+ * When the user right-clicks on a component, a context menu will appear.
+ * This will allow the user to edit the component.
+ */
+function ContextMenu () {
+
+}
+
+export default ContextMenu;
