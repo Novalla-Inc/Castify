ceeb369c07bcbe97b43db8e79124914d94422e35
deondreE <67658523+deondreE@users.noreply.github.com>
2023-04-23 13:08:23 -0500

    basic i18n setup; prettierignore setup

--- desktop/src/services/i18n/i18n.ts
+++ desktop/src/services/i18n/i18n.ts
@@ -1,3 +1,18 @@
-// i18n Config here
+import i18n from "i18next";
+// import fallbackDictionary from "../../i18n/fallback";
+import { initReactI18next } from "react-i18next";
 
-// TODO: Implement this.
+
+/**
+ * TODO: Make it work with the fallback dictionary, and other dictonaries.
+ */
+i18n.use(initReactI18next)
+    .init({
+        // fallbackDictionary,
+        lng: "en",
+        interpolation: {
+            escapeValue: false
+        },
+    })
+
+export default i18n;
